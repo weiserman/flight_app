e37fbbca1056ffc0802c47e40c74a67287502557
Warren <warreneiserman@gmail.com>
2023-02-25 14:40:13 +0000

    base abap repo

---
+++ flight_management/flight_management/doctype/scarr/test_scarr.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, DataGimbal (Pty) Ltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSCARR(FrappeTestCase):
+	pass
---
+++ flight_management/flight_management/doctype/sflight/sflight.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, DataGimbal (Pty) Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SFLIGHT", {
+// 	refresh(frm) {
+
+// 	},
+// });
